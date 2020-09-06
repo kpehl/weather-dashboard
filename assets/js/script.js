bf0f2fe96dd34529ac275b18f4467e5c2eb33502
kpehl <30782087+kpehl@users.noreply.github.com>
2020-09-06 18:22:20 -0400

    added functionality to search form - need to add error handling for incorrect state form; created moment to get current date; modified displayCurrentWeather to create a card and a heading with the city and current date - more weather data to come next

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,32 @@ var apiKey = "86a0171fe8b8a02fbb9273530ba556fd";
 var cityFormEl = document.querySelector("#city-form");
 // An element for the form input
 var cityInputEl = document.querySelector("#city");
+// An element for the current weather container
+var currentWeatherContainerEl = document.querySelector("#current-weather-container");
+// An element for the city returned by the API
+var citySearchTerm = document.querySelector("#city-search-term");
+// An element for the forecast container
+var forecastContainerEl = document.querySelector("#forecast-container");
+
+// A moment for the current date
+var currentDate = moment().format("M/DD/YYYY");
+
 
 // A function to handle the form input
 var formSubmitHandler = function(event) {
     // prevent the default action of submitting the input to an external database
     event.preventDefault();
     console.log(event);
-    // get the value of the city
+    // get the value of the city and clear the form
     var city = cityInputEl.value.trim();
     if (city) {
         getCurrentWeather(city);
         cityInputEl.value = "";
+    // if no value is entered, alert the user
     } else {
         alert("Please enter a city");
     }
 };
-
-// Create a URL for a 5 day forcast query
-var forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=" + apiKey;
 
 // Function to get the current weather data
 var getCurrentWeather = function(city) {
@@ -31,10 +39,33 @@ var getCurrentWeather = function(city) {
     var weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
     fetch(weatherApiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayCurrentWeather(data, city);
         })
     })
 };
+
+// Function to display the current weather data
+var displayCurrentWeather = function(weatherData, searchTerm) {
+    console.log(weatherData);
+    console.log(searchTerm);
+    // clear old content
+    currentWeatherContainerEl.innerHTML = "";
+    // create a container for the current weather
+    var currentWeatherEl = document.createElement("div");
+    currentWeatherEl.classList = "card align-left";
+    // create a heading for the city name
+    var cityName = document.createElement("h3");
+    // get the city name that the API provided from the search
+    cityName.textContent = weatherData.name + " (" + currentDate + ")";
+    // append the name to the container
+    currentWeatherEl.appendChild(cityName);
+    // append the container to the DOM
+    currentWeatherContainerEl.appendChild(currentWeatherEl);
+
+};
+
+// Create a URL for a 5 day forcast query
+var forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=" + apiKey;
 
 // A function to generate the icon image tag
 var getIcon = function(iconCode) {
