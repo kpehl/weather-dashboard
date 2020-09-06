22f2bdacf900884e27902a6d2afe0aaa97400d0e
kpehl <30782087+kpehl@users.noreply.github.com>
2020-09-06 15:21:42 -0400

    API key is active; successfully called weather data for Jacksonville, verified temperatures are in F

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,29 @@ var apiKey = "86a0171fe8b8a02fbb9273530ba556fd";
 // A variable for the city name, initially hard coded for testing
 var city = "Jacksonville";
 
-// Create a url for a current weather query, specifying the city, that imperial units are desired, and adding my API key
-var weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+// Create a URL for a current weather query, specifying the city, that imperial units are desired, and adding my API key
+var weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
 
 // Create a URL for a 5 day forcast query
-var forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+var forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=" + apiKey;
 
 // Function to get the current weather data
 var getCurrentWeather = function() {
     console.log("the get current weather function has been called");
+    fetch(weatherApiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
 };
+
+// A function to generate the icon image tag
+var getIcon = function(iconCode) {
+    // Create a URL for the weather icon provided by the weather data
+    var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+    // Create an icon image element
+    var iconEl = document.createElement("img")
+    iconEl.setAttribute("src", iconUrl)
+}
 
 getCurrentWeather();
