f77f373b997a1db8f46b29ab6bed77ed533ec4e8
kpehl <30782087+kpehl@users.noreply.github.com>
2020-09-06 15:49:47 -0400

    added elements and an event listener for the search button and verified that event listener is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
 // A variable for my personal Open Weather API key
 var apiKey = "86a0171fe8b8a02fbb9273530ba556fd";
 
-// A variable for the city name, initially hard coded for testing
-var city = "Jacksonville";
+// An element for the form
+var cityFormEl = document.querySelector("#city-form");
+// An element for the form input
+var cityInputEl = document.querySelector("#city");
 
-// Create a URL for a current weather query, specifying the city, that imperial units are desired, and adding my API key
-var weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
+// A function to handle the form input
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+}
 
 // Create a URL for a 5 day forcast query
 var forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=" + apiKey;
 
 // Function to get the current weather data
-var getCurrentWeather = function() {
-    console.log("the get current weather function has been called");
+var getCurrentWeather = function(city) {
+    console.log("the get current weather function has been called for " + city);
+    // Create a URL for a current weather query, specifying the city, that imperial units are desired, and adding my API key
+    var weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
     fetch(weatherApiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
@@ -29,4 +36,10 @@ var getIcon = function(iconCode) {
     iconEl.setAttribute("src", iconUrl)
 }
 
-getCurrentWeather();
+// A variable for the city name, initially hard coded for testing
+var city = "Jacksonville";
+
+getCurrentWeather(city);
+
+// Event Listener for the Search Button
+cityFormEl.addEventListener("submit", formSubmitHandler)
